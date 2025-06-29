c2610e84cd49038a6116593da2cd7993a3be0e62
maksym.mahala <maksym.mahala@intenselab.com>
2025-06-29 14:05:50 +0300

    Commit

---
+++ AudioConverter/AudioConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanerApp.swift
+//  Cleaner
+//
+//  Created by Max on 29.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
